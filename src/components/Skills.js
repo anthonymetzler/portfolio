cf0b12705ce58d3bde644fe993e3156a527a574a
Anthony Metzler <anthony.metzler@gmail.com>
2017-06-22 01:25:45 -0700

    Update skills section to MUI

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import Chip from 'material-ui/Chip';
+import {blue500} from 'material-ui/styles/colors';
 
-const style = {margin: 15};
+
+
+const styles = {
+chip: {
+  margin: 4
+},
+skills: {
+  margin: 15
+}
+};
 
 const Skills = props => {
     const getSkills = props.skillsData[0].keywords.map(function(item, index) {
-      return (<li key={index}><span className="label label-success">{item}</span></li>)
+      return (
+      <li key={index}>
+        <Chip 
+            key={item} 
+            backgroundColor={blue500}
+            style={styles.chip} >{item}
+        </Chip></li>)
     });
 
   	return (
-  	  <div style={style}>
+  	  <div style={styles.skills}>
         <h2 className="text-uppercase"><i className="fa fa-lg fa-code"></i> Skills</h2>
         <ul className="skills-list list-inline">{getSkills}</ul>
       </div>
