91c0d6884f671759fda8b44f0e552a97bce620eb
Anthony Metzler <anthony.metzler@gmail.com>
2019-08-20 20:57:40 -0700

    New build

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c4311d0a607cec7c5e9e8362572f9fa9.js"
+  "/portfolio/precache-manifest.44a73785af042cbaf50f62f5f2d3f11e.js"
 );
 
 workbox.clientsClaim();
